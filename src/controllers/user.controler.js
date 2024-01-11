626d901c7af6ece861b52c9b375943f118fd60c5
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2024-01-11 23:46:06 +0530

    loginUser controller

--- src/controllers/user.controler.js
+++ src/controllers/user.controler.js
@@ -4,21 +4,21 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-
-const generateAccessAndRefreshTokens = async(userId) =>{
-try {
-  const user = await User.findOne(userId)
-  const accessToken = user.generateAccessToken();
-  const refreshToken = user.generateRefreshToken();
-  user.refreshToken = refreshToken
-  await user.save({validateBeforeSave:false})
-  return {accessToken , refreshToken}
-
-  
-} catch (error) {
-  throw new ApiError(500,"Something went wrong while generating refresh and access token")
-}
-}
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findOne(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating refresh and access token"
+    );
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from the frontend
@@ -80,41 +80,68 @@ const registerUser = asyncHandler(async (req, res) => {
   return res
     .status(201)
     .json(new ApiResponse(200, createdUser, "User ragistred Succesfully"));
-} );
+});
 
-const loginUser = asyncHandler(async(req,res)=>{
+const loginUser = asyncHandler(async (req, res) => {
   //get email and password from the user
-  //validate the email and password 
-  //check the email is availble in the database 
-  //validat the password with the password stored in the database 
-  //if valid create acces and refresh  token 
+  //validate the email and password
+  //check the email is availble in the database
+  //validat the password with the password stored in the database
+  //if valid create acces and refresh  token
   //send cookies
 
-  const {email , username , password}=req.body
+  const { email, username, password } = req.body;
 
-  if(!username ||  !email) {
-    throw new ApiError(400,"Please provide either username or email")
+  if (!username || !email) {
+    throw new ApiError(400, "Please provide either username or email");
   }
 
-  const user =  await User.findOne({
-    $or:[{username},{email}]
-   })
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
 
-   if(!user) {
-    throw new ApiError(404,"User does not exist")
-   }
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
 
-   const isPasswordValid = await  user.isPasswordCorrect(password)
-   if(!isPasswordValid){
-    throw new ApiError(401,"Invalid user credentials")
-   }
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
 
-    const {accessToken , refreshToken} =await generateAccessAndRefreshTokens(user._id)
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
 
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
-})
+  //now we have to send hte cookies we have to desgign the ooptions which is a object the reson fro this is your cookies can be modified bydefault i the frontedn but wehn it is set the
+  //cookies can only be modified inthe frontend
 
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
 
-export { 
-  registerUser,
-loginUser };
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          refreshToken,
+          accessToken,
+        },
+        "User logged in Successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req ,res))
+
+export { registerUser, loginUser };
